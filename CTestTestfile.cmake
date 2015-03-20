e1ea85cf2fa0619f77f10502d1661db55dd5b16a
Dev <dev@doodads.com>
2015-03-20 16:41:12 -0400

    Cleanup / Hugo Patches

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kver/Development/source/breeze/kdecoration
-# Build directory: /home/kver/Development/source/breeze/kdecoration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
